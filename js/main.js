6112879d8456706b35ead8d64ab8764a4325e8b0
Bravehearts19 <leo18111998@gmail.com>
2021-11-03 11:21:42 +0100

    Created repository and uploaded files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+Vue.config.devtools = true;
+
+/* Funzione-evento che parte alla lettura del contenuto del DOM della pagina */
+/* Viene utilizzata per racchiudere tutte le variabili non rendendole globali */
+window.addEventListener("DOMContentLoaded", function () {
+    /* const vueApp =  */new Vue({
+        el: "#root",
+        data: {
+            
+        },
+        methods: {
+            
+        },
+        /* mounted() {
+         setInterval(() => {
+
+            }, 1000)       
+        } */
+    });
+});
