549a3ad49e4462618646c3747c32967a43ba9ea8
Bravehearts19 <leo18111998@gmail.com>
2021-11-04 15:51:04 +0100

    Tentativi con la milestone 4

---
+++ milestone 4/js/main.js
@@ -0,0 +1,143 @@
+Vue.config.devtools = true;
+
+/* Funzione-evento che parte alla lettura del contenuto del DOM della pagina */
+/* Viene utilizzata per racchiudere tutte le variabili non rendendole globali */
+window.addEventListener("DOMContentLoaded", function () {
+    /* const vueApp =  */new Vue({
+    el: "#root",
+    data: {
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: 'img/avatar_1.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Fabio',
+                avatar: 'img/avatar_2.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Samuele',
+                avatar: 'img/avatar_3.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: 'img/avatar_4.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+        ],
+        activeContact: {},
+        newTextMessage: "",
+        searchText: "",
+    },
+    methods: {
+        onChatClick(chatDaAttivare) {
+            this.activeContact = chatDaAttivare;
+        },
+        onOpenPage() {
+            this.activeContact = this.contacts[0];
+            return this.activeContact;
+        },
+        sendTextMessage() {
+            if (this.newTextMessage !== "") {
+                this.addMessage(this.newTextMessage, "sent");
+                this.newTextMessage = "";
+
+                setTimeout(() => {
+                    this.addMessage("Ok", "received");
+                }, 1000);
+            }
+        },
+        addMessage(messageText, status) {
+            this.activeContact.messages.push({
+                text: messageText,
+                date: '10/01/2020 15:50:00',
+                status: status,
+            });
+        },
+        getFilteredContacts() {
+            return this.contacts.filter((contact) => {
+                let mustReturn = true;
+
+                if ( contact.name.toLowerCase().includes(this.searchText.toLowerCase()) ) {
+                    mustReturn = true;
+                } else {
+                    mustReturn = false;
+                }
+
+                return mustReturn;
+            });
+        }
+    },
+    /* mounted() {
+     setInterval(() => {
+
+        }, 1000)       
+    } */
+});
+});
