f95d3c3d07db548e357c2174ed01d7b1d2d4b961
Bravehearts19 <leo18111998@gmail.com>
2021-11-05 11:47:12 +0100

    Try for milestone 5

--- milestone 5/js/main.js
+++ milestone 5/js/main.js
@@ -94,95 +94,12 @@ window.addEventListener("DOMContentLoaded", function () {
         activeContact: {},
         newTextMessage: "",
         searchText: "",
-        filteredContacts: [
-            {
-                name: 'Michele',
-                avatar: 'img/avatar_1.jpg',
-                visible: true,
-                messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 16:15:22',
-                        text: 'Tutto fatto!',
-                        status: 'received'
-                    }
-                ],
-            },
-            {
-                name: 'Fabio',
-                avatar: 'img/avatar_2.jpg',
-                visible: true,
-                messages: [
-                    {
-                        date: '20/03/2020 16:30:00',
-                        text: 'Ciao come stai?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '20/03/2020 16:30:55',
-                        text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
-                    },
-                    {
-                        date: '20/03/2020 16:35:00',
-                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
-                    }
-                ],
-            },
-            {
-                name: 'Samuele',
-                avatar: 'img/avatar_3.jpg',
-                visible: true,
-                messages: [
-                    {
-                        date: '28/03/2020 10:10:40',
-                        text: 'La Marianna va in campagna',
-                        status: 'received'
-                    },
-                    {
-                        date: '28/03/2020 10:20:10',
-                        text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '28/03/2020 16:15:22',
-                        text: 'Ah scusa!',
-                        status: 'received'
-                    }
-                ],
-            },
-            {
-                name: 'Luisa',
-                avatar: 'img/avatar_4.jpg',
-                visible: true,
-                messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
-                    }
-                ],
-            },
-        ],
+        filteredContacts: [],
     },
     methods: {
-        onChatClick(chatDaAttivare) {
+        onChatClick(chatDaAttivare, i) {
             this.activeContact = chatDaAttivare;
+            this.activeContact.index = i;
         },
         onOpenPage() {
             this.activeContact = this.contacts[0];
@@ -200,29 +117,23 @@ window.addEventListener("DOMContentLoaded", function () {
         },
         addMessage(messageText, status) {
             this.activeContact.messages.push({
+                date: dayjs().format("DD/MM/YYYY HH:mm:ss"),
                 text: messageText,
-                date: '10/01/2020 15:50:00',
                 status: status,
             });
         },
         getFilteredContacts() {
             this.filteredContacts = this.contacts.filter((contact) => {
-                console.log(contact, this.searchText);
-                let mustReturn = true;
-
-                if ( contact.name.toLowerCase().includes(this.searchText.toLowerCase()) ) {
-                    mustReturn = true;
-                } else {
-                    mustReturn = false;
-                }
-
-                return mustReturn;
+                return contact.name.toLowerCase().includes(this.searchText.toLowerCase().trim())
             });
+            return this.filteredContacts;
         }
     },
-    /* created() {
+    befoteMount() {
         this.activeContact = this.contacts[0];
-    }, */
+        this.activeContact.index = 0;
+        getFilteredContacts();
+    },
     /* mounted() {
      setInterval(() => {
 
